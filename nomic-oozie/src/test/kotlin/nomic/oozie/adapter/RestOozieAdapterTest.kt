e41cc2225d67e4a9b3650680d00b2c78bb71e28f
Vrabel, Zdenko <zdenko.vrabel@wirecard.com>
2017-12-13 15:51:03 +0100

    fixed unit test

--- nomic-oozie/src/test/kotlin/nomic/oozie/adapter/RestOozieAdapterTest.kt
+++ nomic-oozie/src/test/kotlin/nomic/oozie/adapter/RestOozieAdapterTest.kt
@@ -40,7 +40,7 @@ public class RestOozieAdapterTest {
 
 			var json = RestOozieAdapterTest::class.java.getResourceAsStream("/oozie-coordinator-resp.json").reader().readText()
 			oozieServer.stubFor(
-				get(urlEqualTo("/oozie/v1/jobs?jobtype=coord&filter=status=RUNNING"))
+				get(urlEqualTo("/oozie/v1/jobs?jobtype=coord&filter=status=WAITING;status=READY;status=SUBMITTED;status=RUNNING;status=SUSPENDED;"))
 					.willReturn(okJson(json))
 			)
 
